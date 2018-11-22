dcdaf0f8a66b6d3131bfd5aade454598836edefd
Ramzi Ouertani <ramzi.ouertani@ext.bpifrance.fr>
2018-11-22 16:19:14 +0100

    adaptation aux normes BPI france

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+insert into individu values('0103567975','BPIFRANCE','0','true','valid','ENTREPRISE');
+insert into individu values('0203567102','VALEO','0','true','temp','ENTREPRISE');
+insert into individu values('0103567250','RATP','0','true','valid','ENTREPRISE');
+
+insert into individu values('010356797500002','FINANCE','0103567975','true','temp','ETABLISSEMENT');
+insert into individu values('010356797500003','CREDIT COUT TERME','0103567975','false','valid','ETABLISSEMENT');
+insert into individu values('020356710200004','MINUISERIE','0203567102','true','temp','ETABLISSEMENT');
+insert into individu values('020356710200005','CONSTRUCTION','0203567102','false','temp','ETABLISSEMENT');
+insert into individu values('020356710200006','ADMINISTRATION','0203567102','false','valid','ETABLISSEMENT');
+insert into individu values('010356725000012','CONSTRUCTION','0103567250','true','valid','ETABLISSEMENT');
+insert into individu values('010356725000013','ADMINISTRATION','0103567250','false','temp','ETABLISSEMENT');
+
+insert into individu values('1458974','omar','010356797500002','false','valid','INTERLOCUTEUR');
+insert into individu values('1454574','DAVID','010356797500002','false','valid','INTERLOCUTEUR');
+insert into individu values('1458789','RAMZI','020356710200005','false','valid','INTERLOCUTEUR');
+insert into individu values('2104574','PASCAL','020356710200005','false','valid','INTERLOCUTEUR');
+insert into individu values('5898974','AHMED','010356725000013','false','valid','INTERLOCUTEUR');
+insert into individu values('3374574','EDITH','010356725000013','false','valid','INTERLOCUTEUR');
+
+insert into individu values('108302','MAISON ALFORT','010356797500002','false','valid','ADRESSE');
+insert into individu values('163302','marseille','010356797500003','false','valid','ADRESSE');
+insert into individu values('101402','lyon','020356710200004','false','valid','ADRESSE');
+insert into individu values('103402','Noisy','020356710200005','false','valid','ADRESSE');
+insert into individu values('101552','Tunis','020356710200006','false','valid','ADRESSE');
+insert into individu values('103582','Londre','010356725000012','false','valid','ADRESSE');
+insert into individu values('101672','Paris','010356725000013','false','valid','ADRESSE');
